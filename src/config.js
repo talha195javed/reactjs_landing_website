b921c812d1cb780d40d1171ebe5478903fd0ddd3
MUHAMMAD TALHA JAVED <80463395+talha195javed@users.noreply.github.com>
2025-04-28 15:45:09 +0400

    stripe changes

---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const config = {
+    STRIPE_PUB_KEY: 'pk_test_51RHioER6TPn3anLsM5dHoX2W8KlhZldusKq8sZKOHpUXPsPU2uUxYcdh8qMYpP73hcCSnHtT6N3pqnF9Hcyx70TM00ngnQRydG',
+    API_URL: 'https://web.smartvisitor.io/'
+};
